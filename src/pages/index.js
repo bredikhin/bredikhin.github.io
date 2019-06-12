ff92874eeb15667637abecbae344a132b40a7212
Ruslan Bredikhin <rus.brn@gmail.com>
2019-06-12 06:19:35 -0400

    add projects and clients

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,125 +1,130 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
+import Client from '../components/Client'
 import Layout from '../components/layout'
 
-class HomeIndex extends React.Component {
-  constructor() {
-    super()
+import logo1 from '../assets/images/client1.png'
+import logo2 from '../assets/images/client2.png'
+import logo3 from '../assets/images/client3.png'
+import logo4 from '../assets/images/client4.png'
+import logo5 from '../assets/images/client5.png'
+import logo6 from '../assets/images/client6.png'
+import logo7 from '../assets/images/client7.png'
 
-    this.state = {
-      lightboxIsOpen: false,
-      currentImage: 0,
-    }
+const clients = [
+  {
+    name: 'Lookbooks',
+    img: logo1,
+    url: 'https://www.lookbooks.com/',
+  },
+  {
+    name: 'Ssense',
+    img: logo2,
+    url: 'https://www.ssense.com/',
+  },
+  {
+    name: 'Waggl',
+    img: logo3,
+    url: 'https://www.waggl.com/',
+  },
+  {
+    name: 'Clevertech',
+    img: logo4,
+    url: 'https://www.clevertech.biz/',
+  },
+  {
+    name: 'Automotive Data Solutions',
+    img: logo5,
+    url: 'https://www.adsdata.ca/',
+  },
+  {
+    name: 'Spiria',
+    img: logo6,
+    url: 'https://www.spiria.com/',
+  },
+  {
+    name: 'Estalio',
+    img: logo7,
+    url: 'http://www.estalio.com/',
+  },
+]
 
-    this.closeLightbox = this.closeLightbox.bind(this)
-    this.gotoNext = this.gotoNext.bind(this)
-    this.gotoPrevious = this.gotoPrevious.bind(this)
-    this.openLightbox = this.openLightbox.bind(this)
-    this.handleClickImage = this.handleClickImage.bind(this)
-  }
+const HomeIndex = () => (
+  <Layout>
+    <Helmet>
+      <title>Ruslan Bredikhin</title>
+      <meta
+        name="description"
+        content="Ruslan Bredikhin, senior software developer from Montreal, Canada"
+      />
+    </Helmet>
 
-  openLightbox(index, event) {
-    event.preventDefault()
-    this.setState({
-      currentImage: index,
-      lightboxIsOpen: true,
-    })
-  }
-  closeLightbox() {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false,
-    })
-  }
-  gotoPrevious() {
-    this.setState({
-      currentImage: this.state.currentImage - 1,
-    })
-  }
-  gotoNext() {
-    this.setState({
-      currentImage: this.state.currentImage + 1,
-    })
-  }
-  handleClickImage() {
-    if (this.state.currentImage === this.props.images.length - 1) return
-
-    this.gotoNext()
-  }
-
-  render() {
-    return (
-      <Layout>
-        <Helmet>
-          <title>Ruslan Bredikhin</title>
-          <meta
-            name="description"
-            content="Ruslan Bredikhin, senior software developer from Montreal, Canada"
-          />
-        </Helmet>
-
-        <div id="main">
-          <section id="one">
-            <header className="major">
-              <h2>Summary</h2>
-            </header>
-            <p>
-              Senior software engineer with over a decade of experience and high
-              level of expertise in all aspects of Web-production including
-              development, testing, deployment and maintenance.
-            </p>
-            <p>
-              <em>
-                While holding a degree in informatics and mathematics, I admit
-                that most of my knowledge comes from the real life experience. I
-                have worked with a lot of software stacks and Web technologies,
-                from LAMP (Linux, MySQL, Apache, PHP) and Ruby on Rails to MEAN
-                (MongoDB, Express.js, Angular.js, Node.js), Golang, Erlang and
-                Elixir. One of the main things I believe in is that keeping
-                oneself up-to-date with the latest trends is the only way to
-                stay on top in such rapidly changing industry as IT.
-              </em>
-            </p>
-          </section>
-          <section id="two">
-            <header className="major">
-              <h2>Technologies</h2>
-            </header>
-            <li>
-              Node.js, Ruby & Ruby on Rails, Elixir, Erlang, Golang, Python, PHP
+    <div id="main">
+      <section id="one">
+        <header className="major">
+          <h2>Summary</h2>
+        </header>
+        <p>
+          Senior software engineer with over a decade of experience and high
+          level of expertise in all aspects of Web-production including
+          development, testing, deployment and maintenance.
+        </p>
+        <p>
+          <em>
+            While holding a degree in informatics and mathematics, I admit that
+            most of my knowledge comes from the real life experience. I have
+            worked with a lot of software stacks and Web technologies, from LAMP
+            (Linux, MySQL, Apache, PHP) and Ruby on Rails to MEAN (MongoDB,
+            Express.js, Angular.js, Node.js), Golang, Erlang and Elixir. One of
+            the main things I believe in is that keeping oneself up-to-date with
+            the latest trends is the only way to stay on top in such rapidly
+            changing industry as IT.
+          </em>
+        </p>
+      </section>
+      <section id="two">
+        <header className="major">
+          <h2>Technologies</h2>
+        </header>
+        <li>
+          Node.js, Ruby & Ruby on Rails, Elixir, Erlang, Golang, Python, PHP
+        </li>
+        <li>PostgreSQL, MongoDB, Redis</li>
+        <li>React.js, Redux, GraphQL, TypeScript, CSS</li>
+        <li>Git, Docker, Kubernetes, Linux shell, Vim, AWS</li>
+      </section>
+      <section id="three">
+        <header className="major">
+          <h2>Projects and Clients</h2>
+        </header>
+        <ul className="clients">
+          {clients.map(client => (
+            <li key={client.name}>
+              <Client {...client} />
             </li>
-            <li>PostgreSQL, MongoDB, Redis</li>
-            <li>React.js, Redux, GraphQL, TypeScript, CSS</li>
-            <li>Git, Docker, Kubernetes, Linux shell, Vim, AWS</li>
-          </section>
-          <section id="three">
-            <header className="major">
-              <h2>Languages</h2>
-            </header>
-            <p>English, French, Spanish, Portuguese, Russian</p>
-          </section>
-          <section id="four">
-            <header className="major">
-              <h2>Education</h2>
-            </header>
-            <h3>Master degree (Hons)</h3>
-            <p>
-              July 2002, Moscow Power Engineering University / Russia
-              <br />
-              Major: Applied Mathematics and Informatics
-            </p>
-            <h3>Bachelor degree (Hons)</h3>
-            <p>
-              July 2000, Moscow Power Engineering University / Russia
-              <br />
-              Major: Applied Mathematics and Informatics
-            </p>
-          </section>
-        </div>
-      </Layout>
-    )
-  }
-}
+          ))}
+        </ul>
+      </section>
+      <section id="four">
+        <header className="major">
+          <h2>Languages</h2>
+        </header>
+        <p>English, French, Spanish, Portuguese, Russian</p>
+      </section>
+      <section id="five">
+        <header className="major">
+          <h2>Education</h2>
+        </header>
+        <h3>Master degree (Hons)</h3>
+        <p>
+          July 2002, Moscow Power Engineering University / Russia
+          <br />
+          Major: Applied Mathematics and Informatics
+        </p>
+      </section>
+    </div>
+  </Layout>
+)
 
 export default HomeIndex
